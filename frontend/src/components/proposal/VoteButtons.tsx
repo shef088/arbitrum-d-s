e88610ab1fedf31c590bd689fbf2ad59a938017b
shef088 <sheriffhamza88@gmail.com>
2024-11-16 13:08:02 +0000

    changes

--- frontend/src/components/proposal/VoteButtons.tsx
+++ frontend/src/components/proposal/VoteButtons.tsx
@@ -11,24 +11,35 @@ interface VoteButtonsProps {
 const VoteButtons: React.FC<VoteButtonsProps> = ({ proposal, handleVote }) => {
     const [votesFor, setVotesFor] = useState<number>(0);
     const [votesAgainst, setVotesAgainst] = useState<number>(0);
+
     useEffect(() => {
-        const proposalUpdate = fetchProposalById(Number(proposal.id))
-        setVotesFor(Number(proposalUpdate.votesFor))
-        setVotesAgainst(Number(proposalUpdate.votesAgainst))
-        console.log("vv:", proposal)
-        console.log("vvupdate:", proposal)
-      }, [proposal]);
-    
+        const fetchProposalDetails = async () => {
+            try {
+                // Fetch updated proposal details
+                const proposalUpdate = await fetchProposalById(Number(proposal.id));
+                if (proposalUpdate) {
+                    // Update state with fetched votes
+                    setVotesFor(Number(proposalUpdate.votesFor));
+                    setVotesAgainst(Number(proposalUpdate.votesAgainst));
+                }
+            } catch (error) {
+                console.error('Error fetching proposal details in votes:', error);
+            }
+        };
+
+        // Call the fetch function
+        fetchProposalDetails();
+    }, [proposal]);  // Trigger on proposal change
+
     return (
         <div className="vote-buttons">
             {!proposal.executed && (
                 <>
                     <button onClick={() => handleVote(true)} className="up-vote-btn">
-                        <FaArrowUp className="vote-icon" /> <span>
-                          {Number(votesFor)}  </span>
+                        <FaArrowUp className="vote-icon" /> <span>{votesFor}</span>
                     </button>
                     <button onClick={() => handleVote(false)} className="down-vote-btn">
-                        <FaArrowDown className="vote-icon" /> <span>{Number(votesAgainst)}</span>
+                        <FaArrowDown className="vote-icon" /> <span>{votesAgainst}</span>
                     </button>
                 </>
             )}
